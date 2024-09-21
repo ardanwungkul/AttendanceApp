c78c48e2f35ff20df266a4c2d50313d1d54e44b7
ardanwungkul <ardanwungkul14@gmail.com>
2024-09-21 22:56:23 +0700

    colors

---
+++ database/migrations/2024_09_21_153630_create_karyawans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('karyawans', function (Blueprint $table) {
+            $table->id();
+            $table->string('karyawan');
+            $table->enum('jenis_kelamin', ['Laki Laki', 'Perempuan']);
+            $table->date('tanggal_lahir');
+            $table->unsignedBigInteger('divisi_id');
+            $table->foreign('divisi_id')->references('id')->on('divisis')->onUpdate('cascade')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('no_hp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('karyawans');
+    }
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: "class",
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                "mineral-green": {
+                    50: "#f5f8f6",
+                    100: "#dee9e4",
+                    200: "#bcd3c9",
+                    300: "#93b5a8",
+                    400: "#6c9586",
+                    500: "#527a6c",
+                    600: "#43655a",
+                    700: "#364f47",
+                    800: "#2e413c",
+                    900: "#293833",
+                    950: "#141f1b",
+                },
             },
         },
     },
