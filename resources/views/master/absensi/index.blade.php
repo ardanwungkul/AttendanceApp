b4ccdd078f5c7eb57241d97dd2d0a282e1ecbad5
dirgaastya <dirga@digits.id>
2024-10-06 02:57:37 +0700

    feat: show attendance by weekly period

--- resources/views/master/absensi/index.blade.php
+++ resources/views/master/absensi/index.blade.php
@@ -1,12 +1,13 @@
 <x-app-layout>
     <x-slot name="header">Riwayat Absensi</x-slot>
-    <div class="pb-4 relative pt-16">
+    <div class="pb-4 relative  pt-8">
         <table class="w-full border rounded-lg overflow-hidden" id="datatable">
             <thead class="bg-mineral-green-200 font-bold">
                 <tr>
                     <td class="!text-center w-10">No</td>
-                    <td class="!text-center w-48">Bulan</td>
-                    <td>Periode</td>
+                    <td class="!text-center">Tahun</td>
+                    <td class="!text-center">Minggu</td>
+                    <td class="!text-center">Rentang Tanggal</td>
                     <td class="!text-center !w-48">Aksi</td>
                 </tr>
             </thead>
@@ -14,19 +15,16 @@
                 @php $no = 1; @endphp
                 @foreach ($groupedAbsensi as $key => $items)
                     @php
-                        // Ekstrak tahun, bulan, dan minggu dari key yang sudah dikelompokkan
-                        $explodedKey = explode('-', $key);
-                        $tahun = $explodedKey[0];
-                        $bulan = $explodedKey[1];
-                        $minggu = $explodedKey[3];
+                        [$tahun, $minggu] = explode('-Minggu-', $key);
                     @endphp
                     <tr>
                         <td class="!text-center">{{ $no++ }}</td>
-                        <td class="!text-center">{{ $bulan }} {{ $tahun }}</td>
-                        <td>Minggu ke-{{ $minggu }}</td>
+                        <td class="!text-center">{{ $tahun }}</td>
+                        <td class="!text-center">Minggu ke-{{ $minggu }}</td>
+                        <td class="!text-center">{{ $rentangTanggal[$key] }}</td>
                         <td class="!text-center">
-                            <a href="{{ route('absensi.show', ['tahun' => $tahun, 'bulan' => $bulan, 'minggu' => $minggu]) }}"
-                                class="text-blue-500 hover:text-blue-700">Lihat Detail</a>
+                            <a href="{{ route('absensi.show', ['tahun' => $tahun, 'minggu' => $minggu]) }}"
+                                class="text-blue-500 hover:underline">Lihat Detail</a>
                         </td>
                     </tr>
                 @endforeach
@@ -40,6 +38,8 @@
         $('#datatable').DataTable({
             info: false,
             lengthChange: false,
+            searching: false,
+            paging: false,
             language: {
                 'search': '',
                 'searchPlaceholder': 'Search for items'
